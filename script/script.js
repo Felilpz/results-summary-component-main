6888170aa4290c494c21a3962f224ad89ca2c751
Felilpz <62868463+Felilpz@users.noreply.github.com>
2023-07-26 10:54:24 -0300

    wip

---
+++ script/script.js
@@ -0,0 +1,13 @@
+// const body = document.getElementById('body')
+// let rodavel = document.getElementById('rodavel')
+
+// body.addEventListener('load', function() {
+//     let numero = 76
+//     for(c = 0; c <= numero; c++) {
+//         rodavel.innerHTML = `${c}`
+//     }
+// })
+
+// setInterval(body, 100)
+
+//dps fazer isso
